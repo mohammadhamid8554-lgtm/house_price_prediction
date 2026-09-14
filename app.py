4117a057ee50a167d50a7b21db5a3283dd8d582e
mohammadhamid8554-lgtm <mohammadhamid8554@gmail.com>
2026-09-15 00:21:25 +0530

    logger and exception handling

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import sys
 from src.ml_house_price_prediction.logger import logging
 from src.ml_house_price_prediction.exception import CustomException
-import sys
-if __name__ == "__main":
+
+if __name__ == "__main__":
     logging.info("The execution has started!!")
 
-try:
-    a = 1 / 0
-except Exception as e:
-    raise CustomException(e, sys) # type: ignore
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Division by zero error occurred.")
+        raise CustomException(e, sys)
